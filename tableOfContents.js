1470ccc3806ac4b8c945e20641269c1fcb35ebff
Maya Mohsin Ahmed <maya.ahmed@gmail.com>
2016-11-23 12:15:25 +0530

    Add files via upload

---
+++ tableOfContents.js
@@ -0,0 +1,56 @@
+
+ var tableDiv = document.getElementById("tableOfContentsDiv");
+Home = "<a href='index.html' class='tocLinks'> Home </a>";
+
+chapter1 = " <br/> <br/> <a href='chapter1.html' class='tocLinks'> 1. A Sister's Wedding </a>";
+chapter2 = " <br\> <a href='chapter2riots.html' class='tocLinks'> 2. Riots. </a>";
+chapter3 = " <br\> <a href='chapter3drum.html' class='tocLinks'>  3. Black Magic with Drumsticks. </a>";
+chapter4 = " <br\> <a href='chapter4frlove.html' class='tocLinks'> 4. A friend in love. </a>";
+chapter5 = " <br\> <a href='chapter5domviol.html' class='tocLinks'> 5. Domestic Violence. </a>";
+chapter6 = " <br\> <a href='chapter6picnic.html' class='tocLinks'> 6. The Class picnic.  </a>";
+chapter7 = " <br\> <a href='chapter7apfool.html' class='tocLinks'> 7. Opening a Pandora's box. </a>";
+chapter8 = "<br\> <a href='chapter8diwali.html' class='tocLinks'>  8. Festival of lights. </a>";
+chapter9 = " <br\> <a href='chapter9floods.html' class='tocLinks'>  9. Bombay monsoons. </a>";
+chapter10 = " <br\> <a href='chapter10bird.html' class='tocLinks'>  10. Caged bird.  </a>";
+chapter11 = " <br\> <a href='chapter11sweets.html' class='tocLinks'> 11. Sweet nothings.  </a>";
+chapter12 = " <br\> <a href='chapter12holi.html' class='tocLinks'> 12. The festival of colors. </a>";
+chapter13 = " <br\> <a href='chapter13sari.html' class='tocLinks'> 13. Shopping around. </a>";
+chapter14 = " <br\> <a href='chapter14namaste.html' class='tocLinks'> 14. Namaste.</a>";
+chapter15 = " <br\> <a href='chapter15goodbye.html' class='tocLinks'> 15. Farewell good friend.</a>";
+chapter16 = " <br\> <a href='chapter16thief.html' class='tocLinks'> 16. The Thief.</a>";
+chapter17 = " <br\> <a href='chapter17contrast.html' class='tocLinks'> 17. The many faces of women.</a>";
+chapter18 = " <br\> <a href='chapter18summer.html' class='tocLinks'> 18. Bombay Summer nights. </a>";
+chapter19 = " <br\> <a href='chapter19train.html' class='tocLinks'> 19. Bombay local trains.</a>";
+chapter20 = " <br\> <a href='chapter20triptotown.html' class='tocLinks'> 20. Trip to town. </a>";
+chapter21 = " <br\> <a href='chapter21songs.html' class='tocLinks'> 21. Songs that touch the heart. </a>";
+chapter22 = " <br\> <a href='chapter22student.html' class='tocLinks'> 22. Tara's student. </a>";
+chapter23 = " <br\> <a href='chapter23husbcand.html' class='tocLinks'> 23. The Husband candidate. </a>";
+chapter24 = " <br\> <a href='chapter24gokul.html' class='tocLinks'> 24.  The Dahi hundi. </a>";
+chapter25 = " <br\> <a href='chapter25bus.html' class='tocLinks'> 25. Home Science. </a>";
+chapter26 = " <br\> <a href='chapter26bank.html' class='tocLinks'> 26. Tara's summer job. </a>";
+chapter27 = " <br\> <a href='chapter27ganesh.html' class='tocLinks'> 27. Where do we go from here? </a>";
+chapter28 = " <br\> <a href='chapter28raji.html' class='tocLinks'> 28. Raji gets married. </a>";
+chapter29 = " <br\> <a href='chapter29winds.html' class='tocLinks'> 29. Winds of change.   </a>";
+chapter30 = " <br\> <a href='chapter30hostel.html' class='tocLinks'> 30. Hostel life.  </a>";
+chapter31 = " <br\> <a href='chapter31politics.html' class='tocLinks'> 31. Keep away from politics.  </a>";
+chapter32 = " <br\> <a href='chapter32discover.html' class='tocLinks'> 32.  Discovering rural India.  </a>";
+chapter33 = " <br\> <a href='chapter33mess.html' class='tocLinks'> 33. Messing around.  </a>";
+chapter34 = " <br\> <a href='chapter34cheeku.html' class='tocLinks'> 34. Outsmarting the dog van.  </a>";
+chapter35 = " <br\> <a href='chapter35seminar.html' class='tocLinks'> 35. Of seminars and presentations. </a>";
+chapter36 = " <br\> <a href='chapter36onam.html' class='tocLinks'> 36. Visiting an orphanage. </a>";
+chapter37 = " <br\> <a href='chapter37pancakes.html' class='tocLinks'> 37. Broadening horizons. </a>";
+chapter38 = " <br\> <a href='chapter38womanswork.html' class='tocLinks'> 38. A woman's work is never done. </a>";
+chapter39 = " <br\> <a href='chapter39rajihome.html' class='tocLinks'> 39. Raji's new world. </a>";
+chapter40 = " <br\> <a href='chapter40mystery.html' class='tocLinks'> 40. Mystery solved. </a>";
+chapter41 = " <br\> <a href='chapter41someonespecial.html' class='tocLinks'> 41. Someone special. </a>";
+chapter42 = " <br\> <a href='chapter42jealous.html' class='tocLinks'> 42. Mine and only mine. </a>";
+chapter43 = " <br\> <a href='chapter43final.html' class='tocLinks'> 43. Tara's wedding. </a>";
+
+function toc(){
+  tableDiv.innerHTML=Home+chapter1+chapter2+ chapter3+chapter4 + chapter5+chapter6 +chapter7+chapter8 +chapter9+chapter10 +chapter11 +chapter12 +chapter13+chapter14 +chapter15+chapter16 +chapter17 +chapter18 +chapter19 +chapter20
++chapter21 +chapter22 +chapter23 +chapter24 +chapter25 +chapter26 +chapter27 +chapter28  +chapter29 
++chapter30 +chapter31 +chapter32 +chapter33 +chapter34 +chapter35 +chapter36 +chapter37 +chapter38  +chapter39 
+    +chapter40 +chapter41  +chapter42     +chapter43;
+ }
+
+toc();
